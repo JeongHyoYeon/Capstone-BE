f1046695c8c399bcdb62228735268938383e69d5
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-01-25 22:51:59 +0900

    initial commit

---
+++ tripfriend/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
