9a503cc503478d6a81b38b70bc65de02614ade25
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-02-07 06:21:16 +0900

    회원가입, 로그인

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
     path('googlelogin/', GoogleLoginView.as_view()),
     path('login/refresh/', TokenRefreshView.as_view()),
     path('logout/', LogoutView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
@@ -7,8 +8,69 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import *
 from datetime import datetime, timedelta
 import requests
+import re
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        # 비밀번호 유효성 검사
+        pw = request.data.get('password')
+        regex_pw = '[A-Za-z0-9!@##$%^&+=]{8,25}'
+        if not re.match(regex_pw, pw):
+            return Response({"8자 이상의 영문 대/소문자, 숫자, 특수문자 조합을 입력해주세요."}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "가입이 성공적으로 이루어졌습니다",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(
+            id=request.data.get("id"), password=request.data.get("password")
+        )
+        if user is not None:
+            serializer = UserSerializer(user)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "로그인에 성공했습니다",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+        else:
+            return Response({"아이디 또는 패스워드 오류입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class GoogleLoginView(APIView):
     def post(self, request):
         payload = {'access_token': request.data.get('token')}  # validate the token
