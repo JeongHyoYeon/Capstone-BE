2d0cc1e29af7da6623160016532b42fe361307e0
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-02-08 22:05:09 +0900

    corsheaders

--- tripfriend/settings/base.py
+++ tripfriend/settings/base.py
@@ -49,9 +49,11 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+)
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'tripfriend.urls'
 
