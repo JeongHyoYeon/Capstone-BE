fe45da5ede485fb49ff13890891d0af0e5c51605
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-02-10 03:05:02 +0900

    get trip list

--- api/views/trip_views.py
+++ api/views/trip_views.py
@@ -3,13 +3,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from ..serializers import *
+from .group_views import GroupView
 
 
 class PersonalTripView(APIView):
     def get(self, request):
-        pass
+        trip_list = Trip.objects.filter(group__user=request.user.id)
+        serializer = TripSerializer(trip_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class GroupTripView(APIView):
     def get(self, request):
-        pass
+        group_list = GroupView.get()
+        trip_list = Trip.objects.filter(group__in=group_list)
+        serializer = TripSerializer(trip_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
