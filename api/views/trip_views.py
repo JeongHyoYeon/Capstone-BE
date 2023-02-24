da7745c6a9026c5ebbc6aa8c9851102c3759cc8f
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-02-25 04:21:44 +0900

    myS3client class

--- api/views/trip_views.py
+++ api/views/trip_views.py
@@ -4,6 +4,34 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from ..serializers import *
 from .group_views import GroupView
+import boto3
+import uuid
+from tripfriend.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME
+
+class MyS3Client:
+    def __init__(self, access_key, secret_key, bucket_name):
+        boto3_s3 = boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
+        )
+        self.s3_client = boto3_s3
+        self.bucket_name = bucket_name
+
+    def upload(self, file):
+        try:
+            file_id = str(uuid.uuid4())
+            extra_args = {'ContentType': file.content_type}
+
+            self.s3_client.upload_fileobj(
+                    file,
+                    self.bucket_name,
+                    file_id,
+                    ExtraArgs=extra_args
+                )
+            return f'https://{self.bucket_name}.s3.ap-northeast-2.amazonaws.com/{file_id}'
+        except:
+            return None
 
 
 class PersonalTripView(APIView):
@@ -19,3 +47,9 @@ class GroupTripView(APIView):
         trip_list = Trip.objects.filter(group__in=group_list)
         serializer = TripSerializer(trip_list, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        s3_client = MyS3Client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME)
+
+
+
--- tripfriend/settings/base.py
+++ tripfriend/settings/base.py
@@ -142,12 +142,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = env('AWS_REGION')
 
 # S3 Storages
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
