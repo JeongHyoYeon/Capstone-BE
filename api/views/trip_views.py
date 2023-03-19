0b971bc46c977a807164c388fd19e7c2536d0081
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-03-19 16:29:01 +0900

    patch trip detail

--- api/views/trip_views.py
+++ api/views/trip_views.py
@@ -95,6 +95,15 @@ class TripDetailView(APIView):
         serializer = TripSerializer(trip)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def patch(self, request, trip):
+        trip_instance = get_object_or_404(Trip, id=trip)
+        serializer = TripSerializer(instance=trip_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
