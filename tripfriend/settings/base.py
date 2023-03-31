4c12f9fd5a2a9a998d4aacf45b87bbdccab556ee
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-03-31 21:08:07 +0900

    Revert "Update base.py"
    This reverts commit e0f88f6c535d7db71279956bbd85dae368d7d537.

--- tripfriend/settings/base.py
+++ tripfriend/settings/base.py
@@ -66,7 +66,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://d2caf2t0hjckii.cloudfront.net"
+    "https://d2caf2t0hjckii.cloudfront.net",
+    "https://app.aftertrip.link"
 )
 
 CORS_ALLOW_CREDENTIALS = True
