168cf6a7b2da32aac13dac00895d27193fa2ec1c
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-04-12 22:41:47 +0900

    폴더 구조 바뀐 것 수정

--- api/views/photo_tag_views.py
+++ api/views/photo_tag_views.py
@@ -9,49 +9,39 @@ from tripfriend.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ST
 
 
 class PhotoTagView(APIView):
+    def get(self, request, part, trip):
+        # TODO: 카테고리 리스트 & 썸네일 리턴
+        if part == 'yolo':
+            pass
+        if part == 'face':
+            pass
+
     def patch(self, request, part, trip):
-        photos = Photo.objects.filter(trip=trip).values_list('id', 'url')
+        photos = Photo.objects.filter(trip=trip).values('id', 'url')
         print(photos)
         trip_object = get_object_or_404(Trip, id=trip)
         # 모델 돌리기 (인자로 url 리스트) -> output: 태그 붙은 딕셔너리
+        # part 인자로 어떤 모델 돌릴지 구분
         # output DB에 저장(수정)
         return Response({"사진 자동 분류가 완료되었습니다."}, status.HTTP_200_OK)
 
 
 class PhotoTagDetailView(APIView):
     def get(self, request, part, trip, tag):
-        # Todo: tag 정보 검색 수정해야 - Yolo 부분 확정되면 고칠 예정
-        if tag == 'scene':
-            # 풍경 안에 있는 카테고리 리스트 리턴
-            # 다시 선택할 경우 이 api 다시 요청
-            photos = Photo.objects.filter(trip=trip)
-            scene_classes = ['buildings', 'forests', 'glacier', 'mountains', 'sea', 'street']
-            data = []
-            for scene in scene_classes:
-                if scene in photos.values_list('tag_yolo__tag_name'):
-                    data.append(
-                        {
-                            "tag": scene,
-                            "thumbnail": Photo.objects.filter(trip=trip, tag_yolo__tag_name=scene)[0]
-                        }
-                    )
-            response = {
-                "status": status.HTTP_200_OK,
-                "data": data
-            }
-            return Response(response)
-
-        else:
-            photos = Photo.objects.filter(trip=trip, tag_yolo__tag_name=tag)
+        if part == 'yolo':
+            photos = Photo.objects.filter(trip=trip, tag_yolo__photos=tag)
+            print(photos)
+            serializer = PhotoReturnSerializer(photos, many=True)
+            return Response(serializer.data, status.HTTP_200_OK)
+        if part == 'face':
+            photos = Photo.objects.filter(trip=trip, tag_face__photos=tag)
             serializer = PhotoReturnSerializer(photos, many=True)
             return Response(serializer.data, status.HTTP_200_OK)
 
+
     def post(self, request, part, trip, tag):
         # 파일 다운로드
-        # trip_photos = Photo.objects.filter(trip=trip)
-        # tag_yolo_list = trip_photos.values_list('tag_yolo__tag_name', flat=True)
-        # # tag_face_list = trip_photos.values_list('tag_face__tag_num', flat=True)
-        # photos = trip_photos.filter(tag__in=tag_yolo_list)
+        # TODO: 폴더 안에 있는 사진만 다운로드 받게 수정
         photos = Photo.objects.filter(trip=trip)  # 다운로드 테스트용
         s3_client = MyS3Client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME)
         for photo in photos:
