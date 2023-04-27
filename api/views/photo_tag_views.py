b37027ea1746851831c30e4800228174f965ea21
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-04-27 16:32:47 +0900

    AI 분류 진행 전 response

--- api/views/photo_tag_views.py
+++ api/views/photo_tag_views.py
@@ -11,10 +11,14 @@ from tripfriend.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ST
 
 class PhotoTagView(APIView):
     def get(self, request, part, trip):
+        trip = get_object_or_404(Trip, id=trip)
         trip_photos = Photo.objects.filter(trip=trip)
         data = []
 
         if part == 'yolo':
+            if trip.yolo_request_num == 0:
+                return Response({"아직 객체 분류가 진행되지 않았습니다."}, status.HTTP_200_OK)
+
             tag_list = trip_photos.values_list('tag_yolo', 'tag_yolo__tag_name')
             for tag in set(tag_list):
                 data.append({
@@ -22,7 +26,12 @@ class PhotoTagView(APIView):
                     "tag": tag[1],
                     "thumbnail": PhotoReturnSerializer(trip_photos.filter(tag_yolo=tag[0]).last()).data
                 })
+
         elif part == 'face':
+
+            if trip.face_request_num == 0:
+                return Response({"아직 인물 분류가 진행되지 않았습니다."}, status.HTTP_200_OK)
+
             tag_list = trip_photos.values_list('tag_face', 'tag_face__custom_name')
             print(set(tag_list))
             for tag in set(tag_list):
@@ -31,6 +40,7 @@ class PhotoTagView(APIView):
                     "tag": tag[1],
                     "thumbnail": PhotoReturnSerializer(trip_photos.filter(tag_face=tag[0]).last()).data
                 })
+
         elif part == 'uploader':
             tag_list = trip_photos.values_list('uploaded_by', 'uploaded_by__name')
             for tag in set(tag_list):
