53a043fe41cd216b94e9053ce33ea9e9c7bfe010
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-04-27 20:14:26 +0900

    apscheduler job 순서 조정

--- api/operator.py
+++ api/operator.py
@@ -3,10 +3,11 @@ from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
 from api.auto_functions import DeletePhotos
 
+
 def start():
     scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
     delete_photos = DeletePhotos()
-    scheduler.add_job(delete_photos.db_delete, CronTrigger.from_crontab('0 0 * * *'))
     scheduler.add_job(delete_photos.s3_delete, CronTrigger.from_crontab('0 0 * * *'))
+    scheduler.add_job(delete_photos.db_delete, CronTrigger.from_crontab('0 0 * * *'))
     scheduler.start()
 
