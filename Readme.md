6bf3274e2a43e8792a3b129796d3bf9097b6eddd
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-02 14:02:42 +0900

    revert models (tag custome name)

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # AfterTrip - 백엔드
 ## ERD
-![image](https://user-images.githubusercontent.com/86969518/235319269-3e0bc6a7-dd7b-4195-9709-ece5d89023ad.png)
+![image](https://user-images.githubusercontent.com/86969518/235579689-081d9b6a-db78-4905-a6d6-2466e5ec540a.png)
--- api/models.py
+++ api/models.py
@@ -95,7 +95,7 @@ class Photo(BaseModel):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     url = models.TextField()
     tag_yolo = models.ManyToManyField('TagYolo', related_name='photos')
-    tag_face = models.ManyToManyField('TagFace', related_name='photos', through='PhotoTagFace')
+    tag_face = models.ManyToManyField('TagFace', related_name='photos')
     taken_at = models.DateTimeField(null=True)
     uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     is_sorted_yolo = models.BooleanField(default=False)
@@ -107,11 +107,6 @@ class TagYolo(models.Model):
 
 class TagFace(models.Model):
     tag_num = models.IntegerField()
-
-
-class PhotoTagFace(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    tagface = models.ForeignKey(TagFace, on_delete=models.CASCADE)
     custom_name = models.CharField(blank=True, max_length=255)
 
 
