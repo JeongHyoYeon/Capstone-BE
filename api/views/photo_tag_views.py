99d6697f36d6cf761a6597ccf1e79f1050e3f1b1
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-10 05:03:48 +0900

    ai 제거 (서버 분리)

--- api/views/photo_tag_views.py
+++ api/views/photo_tag_views.py
@@ -7,8 +7,8 @@ from django.shortcuts import get_object_or_404
 from ..serializers import *
 from api.mys3client import MyS3Client
 from tripfriend.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME
-from face_recognition.face_recognition import face_recognition
-from yolov5.run_yolov5 import run_yolov5
+# from face_recognition.face_recognition import face_recognition
+# from yolov5.run_yolov5 import run_yolov5
 from api.permissions import GroupMembersOnly
 
 
