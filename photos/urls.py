fd6dc436ef115c09152adfeda869f3e76ae482f7
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-13 15:55:10 +0900

    photos url 수정

--- photos/urls.py
+++ photos/urls.py
@@ -4,10 +4,10 @@ from .views import chatgpt_views, photo_views
 urlpatterns = [
     # 사진 api
     # part는 yolo, face 또는 uploader
-    path('photo/<int:trip>/', photo_views.PhotoView.as_view()),
-    path('photo/<str:part>/<int:trip>/', photo_views.PhotoTagView.as_view()),
-    path('photo/<str:part>/<int:trip>/<int:tag>/', photo_views.PhotoTagDetailView.as_view()),
-    path('uploader/<int:trip>/<str:user>/', photo_views.PhotoUploaderDetailView.as_view()),
+    path('<int:trip>/', photo_views.PhotoView.as_view()),
+    path('<str:part>/<int:trip>/', photo_views.PhotoTagView.as_view()),
+    path('<str:part>/<int:trip>/<int:tag>/', photo_views.PhotoTagDetailView.as_view()),
+    path('<int:trip>/<str:user>/', photo_views.PhotoUploaderDetailView.as_view()),
     path('download/<int:photo>/', photo_views.PhotoDownloadView.as_view()),
     # chatGPT api
     path('search/<int:trip>/', chatgpt_views.PhotoSearchView.as_view())
