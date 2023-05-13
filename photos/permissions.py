ad31f4560b5eb885a180a081ab7835c2c54671d9
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-14 05:49:45 +0900

    Update permissions.py

--- photos/permissions.py
+++ photos/permissions.py
@@ -14,7 +14,7 @@ class GroupMembersOnly(BasePermission):
     def has_object_permission(self, request, view, obj):  # trip object 전달
         if request.user.is_authenticated:
             user_groups = Group.objects.filter(usergroup__user_id=request.user.id, usergroup__is_confirmed=True)
-            trip_group = get_object_or_404(Group, id=obj.group)
+            trip_group = get_object_or_404(Group, id=obj.group_id)
             if trip_group in user_groups:
                 return True
             raise PermissionDenied()
