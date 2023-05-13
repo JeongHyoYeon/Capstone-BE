4faba4b9070c92b2c067d5c8930327c683f0cd9a
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-13 15:35:08 +0900

    include trip app

--- tripfriend/settings/base.py
+++ tripfriend/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'trips',
     'photos',
     'rest_framework',
     'rest_framework_simplejwt',
--- tripfriend/urls.py
+++ tripfriend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('trips/', include('trips.urls')),
     path('photos/', include('photos.urls')),
 ]
