0110f2eaddd9cbe1ff5d841ed1868bd987576972
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-15 13:12:16 +0900

    update gpt promtpt

--- photos/views/chatgpt_views.py
+++ photos/views/chatgpt_views.py
@@ -14,34 +14,42 @@ class PhotoSearchView(APIView):
 
     def post(self, request, trip):
         self.check_object_permissions(self.request, obj=get_object_or_404(Trip, id=trip))
-        photos = Photo.objects.filter(trip=trip).values('id', 'uploaded_by__name', 'taken_at',
-                                                        'tag_yolo__tag_name', 'tag_face__custom_name')
-        photo_list = str(photos)
+        photos = Photo.objects.filter(trip=trip).values('id', 'uploaded_by__name', 'taken_at', 'tag_yolo__tag_name',
+                                                        'tag_yolo__tag_name_kr', 'tag_face__custom_name')
+        photo_list = str(list(photos))
         user_input = request.data['user_input']
-        key_desc = "uploaded_by__name는 찍은 사람을 뜻하고 taken_at은 촬영 시간, tag_yolo는 관련 요소를 뜻해"
+        key_desc = "uploaded_by__name is the person who took the photo, taken_at is the date and time of shooting, " \
+                   "tag_yolo__tag_name, tag_yolo__tag_name_kr is an object in the picture, i.e., " \
+                   "a related element, and tag_face__custom_name is the name of the person in the picture"
 
         print(photo_list)
         openai.api_key = OPENAI_KEY
         chatgpt_output = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant who returns file indexes separated by space."},
-                {"role": "user", "content": "다음 리스트에서 해당되는 항목의 file_index만 띄어쓰기로 구분해서 알려줘"},
+                {"role": "system", "content": "You are a helpful assistant who returns ids separated by space."},
+                {"role": "system", "content": "It's okay to be wrong, so use the information given to you "
+                                              "to guess as much as possible and let me know"},
+                {"role": "user", "content": "This is a list of photos and information about each photo"},
                 {"role": "user", "content": photo_list},
                 {"role": "user", "content": key_desc},
                 {"role": "user", "content": user_input}
             ]
         )
-        photo_id_list = chatgpt_output['choices'][0]['message']['content'].split()
-
+        output = chatgpt_output['choices'][0]['message']['content']
         data = []
-        for photo_id in photo_id_list:
-            serializer = PhotoReturnSerializer(get_object_or_404(Photo, file_index=photo_id))
-            data.append(serializer.data)
+        try:
+            photo_id_list = output.split()
+            for photo_id in photo_id_list:
+                serializer = PhotoReturnSerializer(get_object_or_404(Photo, file_index=photo_id))
+                data.append(serializer.data)
 
-        response = {
-            "status": status.HTTP_200_OK,
-            "data": data
-        }
-        return Response(response)
+            response = {
+                "status": status.HTTP_200_OK,
+                "data": data
+            }
+            return Response(response)
+        except Exception as e:
+            print(e)
+            return Response({output})
 
