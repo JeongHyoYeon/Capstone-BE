df71959038cc0e1efb19f89c3ae8fb4346f36713
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-16 14:52:16 +0900

    use BaseModel inTrip

--- trips/models.py
+++ trips/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils.timezone import localdate, localtime
-from accounts.models import Group
+from accounts.models import BaseModel, Group
 
 
 # Create your models here.
-class Trip(models.Model):
+class Trip(BaseModel):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     place = models.CharField(max_length=50)
     departing_date = models.DateField(default=localdate)
