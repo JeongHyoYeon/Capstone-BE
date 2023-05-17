6130d5a403df4ace87268eb88ffbf270725c74fd
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-18 02:18:02 +0900

    celery.py 위치 변경

--- tripfriend/celery.py
+++ tripfriend/celery.py
@@ -1,5 +1,7 @@
 from celery import Celery
+import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tripfriend.settings')
 app = Celery('tripfriend')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
